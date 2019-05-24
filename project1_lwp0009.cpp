9cd818bdca4e30079b58d0fff807b6abe278bde5
Lucas Peacock <lwp0009@auburn.edu>
2019-05-24 07:33:35 -0500

    create header function

--- project1_lwp0009.cpp
+++ project1_lwp0009.cpp
@@ -1,2 +1,30 @@
-#include <iostream>
+#include<iostream>
+#include<iomanip>
+#include<string>
 using namespace std;
+
+int createHeader() {
+	    cout << "******************************************************\n" <<  "Amortization Table\n"
+		 << "******************************************************\n";
+	    cout << left << setw(8) <<"Month" << setw(13) << "Balance" << setw(15) << "Payment Rate" 
+		 << setw(18) << "Interest Principal\n";
+}
+int main() {
+    double loanAmount;
+    int interestRate;
+    int monthlyPayments;
+
+    cout << "Loan Amount: ";
+    cin >> loanAmount;
+    cout << "Interest Rate (% per year): ";
+    cin >> interestRate;
+    cout << "Monthly Payments: ";
+    cin >> monthlyPayments;
+    createHeader();
+    setprecision(2);
+    fixed;
+    cout >> "0" >> "$"+ to_string(loanAmount)
+    return 0;
+
+}
+
