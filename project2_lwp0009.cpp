6901261a0f9862ce4077163d364c1487fb6d4655
Luke Peacock <lwp0009@auburn.edu>
2019-06-01 17:35:57 -0500

    Need to add test drivers

--- project2_lwp0009.cpp
+++ project2_lwp0009.cpp
@@ -33,31 +33,30 @@ bool at_least_two_alive(bool A_alive, bool B_alive, bool C_alive) {
 * Change C_alive into false if Charlie is killed.
 */
 void Aaron_shoots1(bool& B_alive, bool& C_alive){
+
 	int shoot_target_result;
 	if (C_alive) 
 	{
-		shoot_target_result = rand()%100;
- 		if (shoot_target_result <= AARON_SKILL) 
+ 		if (rand()%100 <= AARON_SKILL) 
 		{
-			cout<<"Aaron killed Charlie.\n";
+			//cout<<"Aaron killed Charlie.\n";
  			C_alive = false;
 		}
 		else
 		{
-			cout << "Aaron missed Charlie.\n";
+			//cout << "Aaron missed Charlie.\n";
 		}
 	}
 	else
 	{
-		shoot_target_result = rand()%100;
- 		if (shoot_target_result <= AARON_SKILL) 
+ 		if (rand()%100 <= AARON_SKILL) 
 		{
-			cout<<"Aaron killed Bob.\n";
+			//cout<<"Aaron killed Bob.\n";
  			B_alive = false;
 		}
 		else
 		{
-			cout << "Aaron missed Bob.\n";
+			//cout << "Aaron missed Bob.\n";
 		}
 		
 	}
@@ -71,31 +70,29 @@ void Aaron_shoots1(bool& B_alive, bool& C_alive){
 * Change C_alive into false if Charlie is killed.
 */
 void Bob_shoots(bool& A_alive, bool& C_alive) {
-	int shoot_target_result;
+
 	if (C_alive) 
 	{
-		shoot_target_result = rand()%100;
- 		if (shoot_target_result <= BOB_SKILL) 
+ 		if (rand()%100 <= BOB_SKILL) 
 		{
-			cout<<"Bob killed Charlie.\n";
+			//cout<<"Bob killed Charlie.\n";
  			C_alive = false;
 		}
 		else
 		{
-			cout << "Bob missed Charlie.\n";
+			//cout << "Bob missed Charlie.\n";
 		}
 	}
 	else
 	{
-		shoot_target_result = rand()%100;
- 		if (shoot_target_result <= BOB_SKILL) 
+ 		if (rand()%100 <= BOB_SKILL) 
 		{
-			cout<<"Bob killed Aaron.\n";
+			//cout<<"Bob killed Aaron.\n";
  			A_alive = false;
 		}
 		else
 		{
-			cout << "Bob missed Aaron.\n";
+			//cout << "Bob missed Aaron.\n";
 		}
 	}
 }
@@ -107,15 +104,16 @@ void Bob_shoots(bool& A_alive, bool& C_alive) {
 * Change B_alive into false if Bob is killed.
 */
 void Charlie_shoots(bool& A_alive, bool& B_alive) {
+	
 	if (B_alive) 
 	{
-		cout << "Charlie killed Bob.\n";
+		//cout << "Charlie killed Bob.\n";
 		B_alive = false;			 
 	}
 
 	else
 	{
-		cout<<"Charlie killed Aaron.\n";
+		//cout<<"Charlie killed Aaron.\n";
 		A_alive = false;
 	}
 }
@@ -126,36 +124,33 @@ void Charlie_shoots(bool& A_alive, bool& B_alive) {
 * Return: Change B_alive into false if Bob is killed.
 * */
 void Aaron_shoots2(bool& B_alive, bool& C_alive) {
-	int shoot_target_result;
 	if (C_alive && B_alive) 
 	{
-		cout << "Aaron misses.";
+		//cout << "Aaron misses.";
 		return;
 	}
 	if (C_alive) 
 	{
-		shoot_target_result = rand()%100;
- 		if (shoot_target_result <= AARON_SKILL)
+ 		if (rand()%100 <= AARON_SKILL)
 		{
-			cout<<"Aaron killed Charlie.\n";
+			//cout<<"Aaron killed Charlie.\n";
  			C_alive = false;
 		}
 		else
 		{
-			cout << "Aaron missed Charlie.\n";
+			//cout << "Aaron missed Charlie.\n";
 		}
 	}
 	else
 	{
-		shoot_target_result = rand()%100;
- 		if (shoot_target_result <= AARON_SKILL) 
+ 		if (rand()%100 <= AARON_SKILL) 
 		 {
-			cout<<"Aaron killed Bob.\n";
+			//cout<<"Aaron killed Bob.\n";
  			B_alive = false;
 		}
 		else
 		{
-			cout << "Aaron missed Bob.\n";
+			//cout << "Aaron missed Bob.\n";
 		}
 	}	
 }
@@ -172,21 +167,21 @@ void test_at_least_two_alive(void) {
 	assert(true == at_least_two_alive(true, false, true));
 	cout << "Case passed ...\n";
 }
-void determineVictory(bool A_alive, bool B_alive, bool C_alive, int A_wins, int B_wins, int C_wins)
+void determineVictory(bool A_alive, bool B_alive, bool C_alive, int& A_wins, int& B_wins, int& C_wins)
 {
 	if (A_alive)
 	{
-		cout << "Aaron won the duel." << endl;
+		//cout << "Aaron won the duel." << endl;
 		A_wins++;
 	}
 	else if (B_alive) 
 	{
-		cout << "Bob won the duel." << endl;
+		//cout << "Bob won the duel." << endl;
 		B_wins++;
 	}
 	else if (C_alive)
 	{
-		cout << "Charlie won the duel." << endl;
+		//cout << "Charlie won the duel." << endl;
 		C_wins++;
 	}
 	else
@@ -198,20 +193,19 @@ void determineVictory(bool A_alive, bool B_alive, bool C_alive, int A_wins, int 
 int main() {
 	
 	srand(time(0));
-	bool aaronAlive = true;
-	bool bobAlive = true;
-	bool charlieAlive = true;
+
 	int A_wins = 0;
 	int B_wins = 0;
 	int C_wins = 0;
-	//for (int rounds = 0; rounds < ROUND_AMOUNT; rounds++)
-	//{
-	
-	
+	for (int rounds = 0; rounds < ROUND_AMOUNT; rounds++)
+	{
+		bool aaronAlive = true;
+		bool bobAlive = true;
+		bool charlieAlive = true;
 		do
 		{
 			if(aaronAlive) {
-				Aaron_shoots1(bobAlive, charlieAlive);
+				Aaron_shoots2(bobAlive, charlieAlive);
 			}
 			if(bobAlive) {
 				Bob_shoots(aaronAlive, charlieAlive);
@@ -222,10 +216,13 @@ int main() {
 		} while (at_least_two_alive(aaronAlive, bobAlive, charlieAlive));
 		determineVictory(aaronAlive, bobAlive, charlieAlive, A_wins, B_wins, C_wins);
 
-	//}
-	//cout << "Aaron won " << A_wins << "/" << ROUND_AMOUNT << " duels or %" << fixed << setprecision(2) << A_wins / ROUND_AMOUNT << endl;
-	//cout << "Bob won " << B_wins << "/" << ROUND_AMOUNT << " duels or %" << B_wins / ROUND_AMOUNT << endl; 
-	//cout << "Aaron won " << C_wins << "/" << ROUND_AMOUNT << " duels or %" << C_wins / ROUND_AMOUNT << endl;
+	}
+	double A_per = ((float)A_wins / ROUND_AMOUNT) * 100;
+	double B_per = ((float)B_wins / ROUND_AMOUNT) * 100;
+	double C_per = ((float)C_wins / ROUND_AMOUNT) * 100;
+	cout << "Aaron won " << A_wins << "/" << ROUND_AMOUNT << " duels or %" << fixed << setprecision(2) << A_per << endl;
+	cout << "Bob won " << B_wins << "/" << ROUND_AMOUNT << " duels or %"  << B_per << endl; 
+	cout << "Charlie won " << C_wins << "/" << ROUND_AMOUNT << " duels or %" << C_per << endl;
     
 	return 0;
 }
